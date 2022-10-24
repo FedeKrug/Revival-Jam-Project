d9d021fc98d392e72ff74a3030e8f5e773d3a3cd
FedeKrug <krug_federico@hotmail.com>
2022-10-24 01:32:06 -0300

    Changes in Enemy
    I changed some stuff in enemy script, and it works, but it needs to change direction when the enemy collides with limits of the screen.

--- SpaceShooter_RevivalJam/Assets/Scripts/Enemies/Enemy.cs
+++ SpaceShooter_RevivalJam/Assets/Scripts/Enemies/Enemy.cs
@@ -4,21 +4,97 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    //[SerializeField] List<GameObject> bulletGenerators;
+    [SerializeField] Sprite[] animationSprites;
+    [SerializeField] float animationTime = 1.0f;
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] bool onLimit; 
+    private SpriteRenderer _spriteRenderer;
+    private int _animationFrame;
 
-    Rigidbody2D rb2d;
-    Animator anim;
-    // Start is called before the first frame update
-    void Awake()
+    private Vector3 _direction = Vector2.right;
+
+    private void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //private void Start()
+    //{
+    //    InvokeRepeating(nameof(AnimateSprite), animationTime, animationTime);
+    //}
+
+    //private void AnimateSprite()
+    //{
+    //    _animationFrame++;
+
+    //    if (_animationFrame >= animationSprites.Length)
+    //    {
+    //        _animationFrame = 0;
+    //    }
+
+    //    _spriteRenderer.sprite = animationSprites[_animationFrame];
+    //}
+
+    private void Update()
     {
-        
+        transform.position += _direction * speed * Time.deltaTime;
+
+        //Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        //Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        //foreach (Transform Enemy in transform) 
+        //{
+        //    if (!Enemy.gameObject.activeInHierarchy){
+        //        continue;
+        //    }
+
+        //    if (_direction == Vector3.right && Enemy.position.x >= (rightEdge.x - 1.0f)){
+        //        AdvanceRow();
+        //        break;
+        //    } else if (_direction == Vector3.left && Enemy.position.x < (leftEdge.x + 1.0f)){
+        //        AdvanceRow();
+        //        break;
+        //    }
+        //}
+        if (onLimit)
+		{
+            speed = -1;
+
+		}
+        else if (!onLimit)
+		{
+            speed = 1;
+		}
     }
+
+    //private void AdvanceRow()
+    //{
+    //    _direction.x *= -1.0f;
+
+    //    Vector3 position = this.transform.position;
+    //    position.y -= 1.0f;
+    //    this.transform.position = position;
+
+    //    // Flip the direction the invaders are moving
+    //    _direction = new Vector3(-_direction.x, 0f, 0f);
+    //}
+    private void GoDownARow()
+	{
+        Vector3 position = this.transform.position;
+        position.y -= 1.0f;
+    }
+	//   private void MoveRight()
+	//{
+
+	//}
+	//   private void MoveLeft()
+	//{
+	//       // Flip the direction the invaders are moving
+	//           _direction = new Vector3(-_direction.x, 0f, 0f);
+	//   }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        onLimit = !onLimit;
+	}
 }
---
+++ SpaceShooter_RevivalJam/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    //[SerializeField] List<GameObject> bulletGenerators;
+
+    Rigidbody2D rb2d;
+    Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
